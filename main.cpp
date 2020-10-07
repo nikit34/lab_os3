25e3fe58dc5d24cd13c0bde489daee6a6781fe4c
Никита Пермяков <permikov134@mail.ru>
2020-10-07 08:29:44 +0300

    refactoring and added single proc

--- main.cpp
+++ main.cpp
@@ -3,33 +3,34 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
+#include <chrono>
+#include <thread>
 
 long int * g_size_arr = nullptr;
+long int * g_num_threads = nullptr;
+long int *arr = nullptr;
+long int *arr_single_proc = nullptr;
 
-struct params {
-    long int id_th;
-    long int num_th;
-    long int *arr;
-};
 
-void merge_sort(long int arr[], long int left, long int right);
-void merge(long int arr[], long int left, long int middle, long int right);
+void merge_sort(long int* arr, long int left, long int right);
+void merge(long int* arr, long int left, long int middle, long int right);
 void *thread_merge_sort(void* arg);
-void merge_sections(long int arr[], long int num_thread, long int size_sub_arr, long int aggregation);
-void test_order(long int arr[]);
+void merge_sections(long int* arr, long int num_thread, long int size_sub_arr, long int aggregation);
+void test_order(long int* arr, std::string msg);
 
 int main(int argc, char *argv[]) {
-    long int g_num_threads = atoi(argv[1]);
+    g_num_threads = new long int;
+    *g_num_threads = atoi(argv[1]);
 
     g_size_arr = new long int;
     printf("\n\tCount elements:\t");
     scanf("%ld", g_size_arr);
-    long int *arr = new long int[*g_size_arr];
+    arr = new long int[*g_size_arr];
+    arr_single_proc = new long int[*g_size_arr];
 
-
-    long int size_sub_arr = *g_size_arr / g_num_threads;
+    long int size_sub_arr = *g_size_arr / *g_num_threads;
     struct timeval start, end;
-    double time_spent;
+    double time_spent, time_single_spent;
 
     std::ifstream in("gen_data.txt");
 
@@ -39,43 +40,54 @@ int main(int argc, char *argv[]) {
         // arr[i] = std::to_string(rand() % 100000);
         getline(in, tmp);
         arr[i] = stoi(tmp);
+        arr_single_proc[i] = arr[i];
     }
     in.close();
 
-    pthread_t threads[g_num_threads];
+    // more proc
+    pthread_t threads[*g_num_threads];
     gettimeofday(&start, NULL);
-
-    for (i = 0; i < g_num_threads; ++i) {
-        struct params* p = new params;
-        p->id_th = i;
-        p->arr = arr;
-        p->num_th = g_num_threads;
-        int err = pthread_create(&threads[i], NULL, thread_merge_sort, (void *) p);
+    for (i = 0; i < *g_num_threads; ++i) {
+        int err = pthread_create(&threads[i], NULL, thread_merge_sort, (void *) i);
         if (err){
             printf("ERROR return code from pthread_create() is %d\n", err);
             exit(-1);
         }
     }
-
-    for(i = 0; i < g_num_threads; ++i) {
+    for(i = 0; i < *g_num_threads; ++i)
         pthread_join(threads[i], NULL);
-    }
 
-    merge_sections(arr, g_num_threads, size_sub_arr, 1);
-
+    merge_sections(arr, *g_num_threads, size_sub_arr, 1);
     gettimeofday(&end, NULL);
     time_spent = ((double) ((double) (end.tv_usec - start.tv_usec) / 1000000 + (double) (end.tv_sec - start.tv_sec)));
-    printf("\tTiming: %f s\n", time_spent);
+    printf("\tTime for %ld proccessing: %f s\n", *g_num_threads, time_spent);
 
-    delete g_size_arr;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+    // single proc
+    gettimeofday(&start, NULL);
+    merge_sections(arr_single_proc, 1, *g_size_arr, 1);
+    gettimeofday(&end, NULL);
+    time_single_spent = ((double) ((double) (end.tv_usec - start.tv_usec) / 1000000 + (double) (end.tv_sec - start.tv_sec)));
+    printf("\tTime for single proc: %f s\n", time_single_spent);
+
+
+    printf("\tAcceleration S(n)=T(1)/T(n): %f", time_single_spent / time_spent);
 
     // test
-    test_order(arr);
+    test_order(arr, "muliiproccessing");
+    test_order(arr_single_proc, "singleproccessing");
+
+    delete g_size_arr;
+    delete g_num_threads;
+    delete[] arr;
+    delete[] arr_single_proc;
 
     return 0;
 }
 
-void merge(long int arr[], long int left, long int middle, long int right) {
+void merge(long int* arr, long int left, long int middle, long int right) {
     long int k = 0;
     long int left_length = middle - left + 1;
     long int right_length = right - middle;
@@ -115,7 +127,7 @@ void merge(long int arr[], long int left, long int middle, long int right) {
     }
 }
 
-void merge_sections(long int arr[], long int num_thread, long int size_sub_arr, long int aggregation) {
+void merge_sections(long int* arr, long int num_thread, long int size_sub_arr, long int aggregation) {
     long int left;
     long int right;
     long int middle;
@@ -133,7 +145,7 @@ void merge_sections(long int arr[], long int num_thread, long int size_sub_arr, 
     }
 }
 
-void merge_sort(long int arr[], long int left, long int right) {
+void merge_sort(long int* arr, long int left, long int right) {
     long int middle;
     if (left < right) {
         middle = left + (right - left) / 2;
@@ -144,15 +156,11 @@ void merge_sort(long int arr[], long int left, long int right) {
 }
 
 void *thread_merge_sort(void* arg) {
-    struct params * p = (struct params *)arg;
-    long int thread_id = p->id_th;
-    long int *arr = p->arr;
-    long int num_threads = p->num_th;
-    long int left = thread_id * (*g_size_arr / num_threads);
-    long int right = (thread_id + 1) * (*g_size_arr / num_threads) - 1;
-    if (thread_id == num_threads - 1) {
-        right += (*g_size_arr % num_threads);
-    }
+    long int thread_id = (long int)arg;
+    long int left = thread_id * (*g_size_arr / *g_num_threads);
+    long int right = (thread_id + 1) * (*g_size_arr / *g_num_threads) - 1;
+    if (thread_id == *g_num_threads - 1)
+        right += (*g_size_arr % *g_num_threads);
     long int middle = left + (right - left) / 2;
     if (left < right) {
         merge_sort(arr, left, right);
@@ -162,19 +170,20 @@ void *thread_merge_sort(void* arg) {
     pthread_exit(nullptr);
 }
 
-void test_order(long int arr[]) {
-    long int max = arr[0];
+void test_order(long int* arr, std::string msg) {
     long int i;
+    bool noerror = true;
+    printf("\n\tTests: \n");
     for (i = 1; i < *g_size_arr; ++i) {
-        if (arr[i] >= max) {
-            max = arr[i];
-        } else {
-            printf("Error Index %ld: prev %ld > current %ld\n", i, arr[i-1], arr[i]);
-            return;
+        if (arr[i] < arr[i-1]) {
+            printf("[ %s ] Error index %ld: prev %ld > current %ld\n", msg.c_str(), i, arr[i-1], arr[i]);
+            noerror = false;
         }
     }
-    for (i = 1; i < *g_size_arr; ++i) {
-        printf("%ld\n", arr[i]);
-    }
-    printf("\tArray is in sorted order\n");
+    if (noerror)
+        printf("\n\tArray is in sorted order\n");
+
+    // for (i = 1; i < *g_size_arr; ++i) {
+    //     printf("%ld\n", arr[i]);
+    // }
 }
